78ee7ca46644921fd9e47bb0f18aeb34d121a7d1
jimmyLi <liviu.c.simion@gmail.com>
2016-05-02 06:23:42 +0200

    added angular for all tabs

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -5,12 +5,24 @@ console.log('\'Allo \'Allo!');
 (function () {
 	var app = angular.module('store', []);
 	app.controller('StoreController', function () {
-		this.product = tab;
+		this.products = tabs;
 	});
 
-	var tab = {
+	var tabs = [{
 		title: 'Pink Shoes',
 		subtitle: 'Now at $145.99'
-	};
+	}, {
+		title: 'Anna Field',
+		subtitle: 'Limited Edition'
+	}, {
+		title: 'Prada',
+		subtitle: 'Summer is coming'
+	}, {
+		title: 'Casadei',
+		subtitle: 'All collors available'
+	}, {
+		title: 'Mellow Yellow',
+		subtitle: 'Free delivery'
+	}];
 })();
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,23 @@ console.log('\'Allo \'Allo!');
 (function(){
 var app = angular.module('store',[]);
 app.controller('StoreController', function(){
-	this.product = tab;
+	this.products = tabs;
 });
 
-var tab = {
-	title: 'Pink Shoes',
-	subtitle: 'Now at $145.99',
-}
+var tabs = [{
+		title: 'Pink Shoes',
+		subtitle: 'Now at $145.99',
+	},{
+		title: 'Anna Field',
+		subtitle: 'Limited Edition',
+	},{
+		title: 'Prada',
+		subtitle: 'Summer is coming',
+	},{
+		title: 'Casadei',
+		subtitle: 'All collors available',
+	},{
+		title: 'Mellow Yellow',
+		subtitle: 'Free delivery',
+	}];
 })();
