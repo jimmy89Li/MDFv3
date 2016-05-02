e25fa5db4b6cb88a0a3c89a1f14ad9ba8d6fdf18
jimmyLi <liviu.c.simion@gmail.com>
2016-05-02 07:09:17 +0200

    added items lists with angular and filled in all tabs with images

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -4,6 +4,7 @@ console.log('\'Allo \'Allo!');
 
 (function () {
 	var app = angular.module('store', []);
+
 	app.controller('StoreController', function () {
 		this.products = tabs;
 	});
@@ -23,6 +24,36 @@ console.log('\'Allo \'Allo!');
 	}, {
 		title: 'Mellow Yellow',
 		subtitle: 'Free delivery'
+	}];
+
+	app.controller('NewItemsListController', function () {
+		this.items = newShoes;
+	});
+
+	var newShoes = [{
+		name: 'Item 1',
+		image: 'images/item1.png'
+	}, {
+		name: 'Item 2',
+		image: 'images/item2.png'
+	}, {
+		name: 'Item 3',
+		image: 'images/item3.png'
+	}];
+
+	app.controller('BestItemsController', function () {
+		this.items = bestShoes;
+	});
+
+	var bestShoes = [{
+		name: 'Item 4',
+		image: 'images/item4.png'
+	}, {
+		name: 'Item 5',
+		image: 'images/item5.png'
+	}, {
+		name: 'Item 6',
+		image: 'images/item6.png'
 	}];
 })();
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ console.log('\'Allo \'Allo!');
 
 (function(){
 var app = angular.module('store',[]);
+
 app.controller('StoreController', function(){
 	this.products = tabs;
 });
@@ -22,4 +23,40 @@ var tabs = [{
 		title: 'Mellow Yellow',
 		subtitle: 'Free delivery',
 	}];
+
+app.controller('NewItemsListController', function(){
+	this.items = newShoes;
+});
+
+var newShoes = [
+	{
+		name: 'Item 1',
+		image: 'images/item1.png',
+	},{
+		name: 'Item 2',
+		image: 'images/item2.png',
+	},{
+		name: 'Item 3',
+		image: 'images/item3.png'
+	}
+
+];
+
+app.controller('BestItemsController', function(){
+	this.items = bestShoes;
+});
+
+var bestShoes = [
+	{
+		name: 'Item 4',
+		image: 'images/item4.png'
+	},{
+		name: 'Item 5',
+		image: 'images/item5.png'
+	},{
+		name: 'Item 6',
+		image: 'images/item6.png'
+	},
+];
+
 })();
